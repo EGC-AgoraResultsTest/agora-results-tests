bc9fae0da97a9d3a5e5ef2fc45154b69d2a5a6c2
DrXoo <domgromero@gmail.com>
2015-12-01 16:34:02 +0100

    Añadidos comentarios en utils.py
    Signed-off-by: DrXoo <domgromero@gmail.com>

--- src/main/python/utils.py
+++ src/main/python/utils.py
@@ -2,7 +2,8 @@ import os
 import json
 
 def executeAgoraResults(tally):
-
+	# Ejecutamos Agora-Results con el caso a prueba a probar.
+  	# El resultado lo guardamos en un archivo, en formato Json
 	os.system('agora-results -t res/'+tally+'.tar.gz -s > results')		
 
 def getJsonObjectFromResults():
