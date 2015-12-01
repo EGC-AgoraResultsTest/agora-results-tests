016abfcf6b9f5cf8f071b34ea42d6f33a4bc61ee
tonigallego <tonigallego87@gmail.com>
2015-12-01 11:43:45 +0100

    Añadido el test 06
    Se ha añadido también el correspondiente tar.gz

--- test5To8.py
+++ test5To8.py
@@ -23,6 +23,29 @@ class Test5To8(unittest.TestCase):
 		self.assertEqual(jsonObjectTotalVotes["valid_votes"], 0)
 		self.assertEqual(jsonObjectTotalVotes["blank_votes"], 0)
 		self.assertEqual(jsonObjectTotalVotes["null_votes"], 0)
+
+	# Prueba en la que no haya ganador si no hay votos
+	# Asignado a: test-no_winner_with_no_votes.tar.gz
+	def test_no_winner_with_no_votes(self):
+		os.system('agora-results -t testCases/test-no_winner_with_no_votes.tar.gz -s > results')		
+
+		f = open("results", "r")
+		data = f.read()
+		f.close()	
+
+		jsonObject = json.loads(data)
+
+		jsonObjectTotalVotes = jsonObject["questions"][0]["totals"]
+
+		jsonObjectAnswers = jsonObject["questions"][0]["answers"]
+
+		self.assertEqual(jsonObjectAnswers[0]["winner_position"], None)
+		self.assertEqual(jsonObjectAnswers[1]["winner_position"], None)
+		self.assertEqual(jsonObjectAnswers[2]["winner_position"], None)
+
+		self.assertEqual(jsonObjectTotalVotes["valid_votes"], 0)
+		self.assertEqual(jsonObjectTotalVotes["blank_votes"], 0)
+		self.assertEqual(jsonObjectTotalVotes["null_votes"], 0)
 		
 if __name__=='__main__':
    unittest.main()
