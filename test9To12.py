3c7f7d337d2e1bda41a9ce0645ba4e5b08249755
Joseda13 <joseda.martin@gmail.com>
2015-12-01 12:43:47 +0100

    Creación del archivo test9To12.py
    Se ha añadido el archivo test9To12.py, en el que se incluye el test número 9. Además, se ha añadido el tar.gz con los votos para la prueba correspondiente.

---
+++ test9To12.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+import os
+import json
+import unittest 
+
+
+class Test5To8(unittest.TestCase):
+
+	# Prueba en la que existe un ganador bajo una situación normal
+	# Asignado a: test-winner_in_ordinary_situation.tar.gz
+	def test_winner_in_ordinary_situation(self):
+		os.system('agora-results -t testCases/test-winner_in_ordinary_situation.tar.gz -s > results')		
+
+		f = open("results", "r")
+		data = f.read()
+		f.close()	
+
+		jsonObject = json.loads(data)
+
+		jsonObjectTotalVotes = jsonObject["questions"][0]["totals"]
+
+		jsonObjectAnswers = jsonObject["questions"][0]["answers"]
+
+		self.assertEqual(jsonObjectAnswers[0]["winner_position"], 0)
+		self.assertEqual(jsonObjectAnswers[1]["winner_position"], None)
+		self.assertEqual(jsonObjectAnswers[2]["winner_position"], None)
+
+		self.assertEqual(jsonObjectAnswers[0]["text"], "Opcion 2")
+
+		self.assertEqual(jsonObjectTotalVotes["valid_votes"], 8)
+		self.assertEqual(jsonObjectTotalVotes["blank_votes"], 1)
+		self.assertEqual(jsonObjectTotalVotes["null_votes"], 1)
+
+if __name__=='__main__':
+   unittest.main()
