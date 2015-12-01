54b7b46815e5683b1764f8c9a05184208e618415
DrXoo <domgromero@gmail.com>
2015-12-01 16:34:40 +0100

    Cambiados funcionamientos de las funciones tests
    Llaman a las funciones auxiliares creadas en utils.py
    
    Signed-off-by: DrXoo <domgromero@gmail.com>

--- src/unittest/python/1To4_tests.py
+++ src/unittest/python/1To4_tests.py
@@ -12,19 +12,10 @@ class OneToFour_Tests(unittest.TestCase):
 	# Prueba de un caso en el que todos los votos sean válidos
 	# Asociado a: test_01.tar.gz
 	def test_01(self):
-		# Ejecutamos Agora-Results con el caso a prueba a probar.
-  		# El resultado lo guardamos en un archivo, en formato Json
-		os.system('agora-results -t res/test_01.tar.gz -s > results')		
 
-		# Abrimos el archivo previamente guardado con el resultado de la prueba
-		f = open("results", "r")
-		# Leemos el archivo
-		data = f.read()
-		# Cerramos el archivo
-		f.close()	
+		utils.executeAgoraResults('test_01')	
 
-		# Cargamos como objeto Json los datos del archivo
-		jsonObject = json.loads(data)
+		jsonObject = utils.getJsonObjectFromResults()
 
 		jsonObjectTotalVotes = jsonObject["questions"][0]["totals"]
 
@@ -35,13 +26,10 @@ class OneToFour_Tests(unittest.TestCase):
 	# Prueba en el que todos los vatos sean en blanco
 	# Asociado a: test_02.tar.gz
 	def test_02(self):
-		os.system('agora-results -t res/test_02.tar.gz -s > results')		
 
-		f = open("results", "r")
-		data = f.read()
-		f.close()	
+		utils.executeAgoraResults('test_02')	
 
-		jsonObject = json.loads(data)
+		jsonObject = utils.getJsonObjectFromResults()
 
 		jsonObjectTotalVotes = jsonObject["questions"][0]["totals"]
 
@@ -52,13 +40,10 @@ class OneToFour_Tests(unittest.TestCase):
 	# Prueba en el que todos los votos son en nulo
 	# Asociado a: test_03.tar.gz
 	def test_03(self):
-		os.system('agora-results -t res/test_03.tar.gz -s > results')		
 
-		f = open("results", "r")
-		data = f.read()
-		f.close()	
+		utils.executeAgoraResults('test_03')	
 
-		jsonObject = json.loads(data)
+		jsonObject = utils.getJsonObjectFromResults()
 
 		jsonObjectTotalVotes = jsonObject["questions"][0]["totals"]
 
@@ -69,13 +54,10 @@ class OneToFour_Tests(unittest.TestCase):
 	# Prueba en el que haya una variedad de votos (nulos, blancos y válidos)
 	# Asociado a: test_04.tar.gz
 	def test_04(self):
-		os.system('agora-results -t res/test_04.tar.gz -s > results')		
+		
+		utils.executeAgoraResults('test_04')	
 
-		f = open("results", "r")
-		data = f.read()
-		f.close()	
-
-		jsonObject = json.loads(data)
+		jsonObject = utils.getJsonObjectFromResults()
 
 		jsonObjectTotalVotes = jsonObject["questions"][0]["totals"]
 
