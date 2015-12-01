bb8d2f5047ce077c9243452114829f0153430bcc
tonigallego <tonigallego87@gmail.com>
2015-12-01 18:00:20 +0100

    Se ha añadido el test 15
    Además se ha agregado el tar.gz correspondiente.

--- 13To16.py
+++ 13To16.py
@@ -50,6 +50,29 @@ class Test13To16(unittest.TestCase):
 		self.assertEqual(jsonObjectTotalVotes["blank_votes"], 0)
 		self.assertEqual(jsonObjectTotalVotes["null_votes"], 3)
 
+	# Prueba de votos válidos en una situación en la que min=2 y max=4
+	# Asignado a: test-variety_votes_with_min2max4.tar.gz
+	def test_variety_votes_in_min2max4_situation(self):
+		os.system('agora-results -t testCases/test-variety_votes_with_min2max4.tar.gz -s > results')		
+
+		f = open("results", "r")
+		data = f.read()
+		f.close()	
+
+		jsonObject = json.loads(data)
+
+		jsonObjectTotalVotes = jsonObject["questions"][0]["totals"]
+
+		jsonObjectAnswers = jsonObject["questions"][0]["answers"]
+
+		self.assertEqual(jsonObjectAnswers[0]["winner_position"], 0)
+		self.assertEqual(jsonObjectAnswers[1]["winner_position"], None)
+
+		self.assertEqual(jsonObjectAnswers[0]["text"], "Opcion 2")
+
+		self.assertEqual(jsonObjectTotalVotes["valid_votes"], 4)
+		self.assertEqual(jsonObjectTotalVotes["blank_votes"], 0)
+		self.assertEqual(jsonObjectTotalVotes["null_votes"], 4)
 	
 if __name__=='__main__':
    unittest.main()
