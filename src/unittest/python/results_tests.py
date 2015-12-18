120a8b6670d0e03f0a25af3217729f701249e6ba
DrXoo <domgaromero@gmail.com>
2015-12-18 15:06:03 +0100

    Modificada funcionalidad en results_tests.py
    Signed-off-by: DrXoo <domgaromero@gmail.com>

--- src/unittest/python/results_tests.py
+++ src/unittest/python/results_tests.py
@@ -7,13 +7,17 @@ import unittest
 
 import utils
 
-import agora_results.pipes.results
+from agora_results.pipes.results import do_tallies
 
 class results_tests(unittest.TestCase):
 	
 	def test_positive_check_config(self):
 		config = utils.getConfigJson("res/config.json")
-		self.assertTrue(config)
+		self.assertTrue(do_tallies.check_config(config))
+
+	def test_positive_execute(self):
+		config = utils.getConfigJson("res/config.json")
+		data = "574.tar.gz"
 
 if __name__=='__main__':
    unittest.main()
