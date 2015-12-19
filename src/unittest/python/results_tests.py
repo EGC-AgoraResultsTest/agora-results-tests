819ce5d870c2d238fd4ce71a199393b2d42fffd3
Joseda13 <joseda.martin@gmail.com>
2015-12-19 19:52:11 +0100

    Cambiada función en utils.py
    Se ha modificado el método al que llamaban los tests para hacer que tenga un parámetro opcional que indicaría la configuración.

--- src/unittest/python/results_tests.py
+++ src/unittest/python/results_tests.py
@@ -12,11 +12,11 @@ from agora_results.pipes.results import do_tallies
 class results_tests(unittest.TestCase):
 	
 	def test_positive_check_config(self):
-		config = utils.getConfigJson("res/config.json")
+		config = utils.getConfigJson("res/config/config.json")
 		self.assertTrue(do_tallies.check_config(config))
 
 	def test_positive_execute(self):
-		config = utils.getConfigJson("res/config.json")
+		config = utils.getConfigJson("res/config/config.json")
 		data = "574.tar.gz"
 
 if __name__=='__main__':
