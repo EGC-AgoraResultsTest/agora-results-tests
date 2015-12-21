e644f8f9cb51e4a50f5486d6b2563c640d392ddf
Joseda13 <joseda.martin@gmail.com>
2015-12-21 18:36:20 +0100

    Añadido test unitario negativo a modifications_tests.py
    Se ha añadido un test unitario negativo para los test sobre modifications.
    
    Signed-off-by: Joseda13 <joseda.martin@gmail.com>

--- src/unittest/python/modifications_tests.py
+++ src/unittest/python/modifications_tests.py
@@ -16,6 +16,9 @@ class modifications_tests(unittest.TestCase):
 		config = json.loads('{"data_list": [0,1,2,3,4], "modifications":[0,1,2] }')
 		self.assertTrue(apply_modifications.check_config(config))
 
+	def test_negative_check_config(self):
+		config = json.loads('{"data_list": true}')
+		self.assertRaises(jsonschema.exceptions.ValidationError, apply_modifications.check_config, config)
 
 if __name__=='__main__':
    unittest.main()
