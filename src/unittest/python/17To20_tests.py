74a3ceb5424af2d39d3f4c45c56cfeaea75c9f01
DrXoo <domgaromero@gmail.com>
2015-12-23 17:50:12 +0100

    Modificados funciones que no estaban perfectamente implementadas en los test 17 y 18
    Signed-off-by: DrXoo <domgaromero@gmail.com>

--- src/unittest/python/17To20_tests.py
+++ src/unittest/python/17To20_tests.py
@@ -11,22 +11,31 @@ class Test17To20(unittest.TestCase):
 	# Prueba que el máximo de ganadores no puede ser 0
 	# Asignado a: test_17.tar.gz
 	def test_max_winners_cant_be_0(self):
-		self.assertRaises(ValueError, utils.executeAgoraResults, 'test_17')	
+		utils.executeAgoraResults('test_17')
+		self.assertRaises(ValueError, utils.getJsonObjectFromResults)	
 
 	# Prueba que el máximo de ganadores no puede ser mayor que el número de propuestas
 	# Asignado a: test_18.tar.gz
 	def test_max_winners_cant_be_greater_than_number_of_options(self):
-		self.assertRaises(ValueError, utils.executeAgoraResults, 'test_18')	
+		utils.executeAgoraResults('test_18')
+		jsonData = utils.getJsonObjectFromResults()
+
+		max_winners = jsonData["questions"][0]["num_winners"]
+		number_options = len(jsonData["questions"][0]["answers"])
+
+		self.assertRaises(AssertionError, self.assertTrue, number_options > max_winners)	
 
 	# Prueba que el mínimo de opciones a seleccionar no sea menor que el mínimo
 	# Asignado a: test_19.tar.gz
 	def test_min_cant_be_greater_than_max(self):
-		self.assertRaises(ValueError, utils.executeAgoraResults, 'test_19')	
+		utils.executeAgoraResults('test_19')
+		self.assertRaises(ValueError, utils.getJsonObjectFromResults)	
 
 	# Prueba que el máximo y el mínimo de opciones a seleccionar no sean cero
 	# Asignado a: test_20.tar.gz
 	def test_max_and_min_cant_be_0(self):
-		self.assertRaises(ValueError, utils.executeAgoraResults, 'test_20')	
+		utils.executeAgoraResults('test_20')
+		self.assertRaises(ValueError, utils.getJsonObjectFromResults)	
 
 	
 if __name__=='__main__':
